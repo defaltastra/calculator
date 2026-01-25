838987bd5f38f0364a247b3d95c0a1ce42d9cfa6
Hind_bencheikh <hind.bc05@gmail.com>
2026-01-25 17:02:55 +0100

    change the whole method just because the first one was working but it doesnt make the * or / on prioritie like real calculator

--- index.js
+++ index.js
@@ -1,57 +1,19 @@
-let currentInput = '';
-let currentOperation = '';
-let preveiousInput = '';
+let screen =document.getElementById("screen");
 
-function appendNumber(number){
-    currentInput += number;
-    document.getElementById('screen').value = `${preveiousInput} ${currentOperation} ${currentInput}`;
-
+function appendNumber(num){
+    screen.value+=num;
 }
-
-function appendOperation(operation){
-    if(currentInput === '') return;
-    if(preveiousInput !==''){
-        calculate();
-    }
-    currentOperation = operation;
-    preveiousInput=currentInput;
-    currentInput='';
-    document.getElementById('screen').value=`${preveiousInput} ${currentOperation}`;
+function appendOperation(oper){
+    screen.value+=oper;
 }
-
 function calculate(){
-    if(preveiousInput ==='' || currentInput==='')return;
-    let result;
-    let prev=parseFloat(preveiousInput);
-    let current =parseFloat(currentInput);
-    switch(currentOperation){
-        case '+':
-            result=prev+current;
-            break;
-        case '-':
-            result=prev-current;
-            break;
-        case '*':
-            result=prev*current;
-            break;
-        case '/':
-            if (current===0){
-                alert("Cannot divide by ZERO !");
-                return;
-            }
-                result=prev/current;
-            break;
-        default:
-            return;
-    }
-    currentInput=result.toString();
-    currentOperation='';
-    preveiousInput='';
-    document.getElementById('screen').value=currentInput;
+    try{
+        screen.value=eval(screen.value);
+}
+    catch(error){
+        screen.value="Error";
+}
 }
 function Reset(){
-    currentInput ='';
-    preveiousInput='';
-    currentOperation='';
-    document.getElementById('screen').value='';
+    screen.value="";
 }
